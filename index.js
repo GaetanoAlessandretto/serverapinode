5ae56a6b76c19415599b8d50431e9b215872ab62
Gaetano <gaetano.alessandretto@gmail.com>
2024-02-29 11:19:01 +0100

    feat(master): node js server that calls an api and returns it in json

---
+++ index.js
@@ -0,0 +1,24 @@
+var https = require('https');
+const express = require('express')
+
+const app = express();
+
+app.get("/products", function (req, res) {
+    const url = "https://mockend.up.railway.app/api/products";
+
+    https.get(url, function (response) {
+        let data = '';
+
+        response.on('data', function (chunk) {
+            data += chunk;
+        });
+
+        response.on('end', function () {
+            res.json(data);
+        });
+    });
+});
+
+app.listen(3009, function () {
+    console.log("Server is running on port 3009.");
+});
